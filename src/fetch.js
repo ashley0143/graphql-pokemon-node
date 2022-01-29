e0c1a62b5346d6f4b7071693336a4ecb472b02cf
Ashley <iamashley@tuta.io>
2022-01-29 18:25:05 +0300

    Create fetch.js

---
+++ src/fetch.js
@@ -0,0 +1,22 @@
+import fetch from "node-fetch";
+
+async function get(fetcher, options) {
+  if (!options) var options = "sprite,num,species,color";
+  return await fetch("https://graphqlpokemon.favware.tech/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+      {
+        getPokemon(pokemon: ${fetcher}) {
+        ${options}
+            }
+      }
+    `,
+    }),
+  }).then((res) => res.json());
+}
+
+export default get;
